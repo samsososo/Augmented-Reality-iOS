caf97c4e2ad457623b19ad997baec621dad0bd83
sam.y.so <sam.y.so@accenture.com>
2021-03-14 19:09:39 +0800

    Initial Commit

---
+++ Augmented Reality/Augmented_RealityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Augmented_RealityApp.swift
+//  Augmented Reality
+//
+//  Created by sam.y.so on 14/3/2021.
+//
+
+import SwiftUI
+
+@main
+struct Augmented_RealityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Augmented Reality/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Augmented Reality
+//
+//  Created by sam.y.so on 14/3/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
