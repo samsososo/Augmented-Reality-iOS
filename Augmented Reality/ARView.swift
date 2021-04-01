dcb40908232a7fb92fbc61e38e6fa4fea16388c9
SamSo <49915235+samsososo@users.noreply.github.com>
2021-04-01 09:53:01 +0800

    feat: create and implement the browsing container and item (#2)
    * create a browse view in AR screen
    
    * create a button for add or discard the 3D models
    
    * save all changes
    
    * add three image
    
    * update the browsing item
    
    Co-authored-by: sam.y.so <sam.y.so@accenture.com>

--- Augmented Reality/ARView.swift
+++ Augmented Reality/ARView.swift
@@ -10,21 +10,53 @@ import SwiftUI
 import RealityKit
 
 struct MainARView: View {
+    @EnvironmentObject var placementSettings: PlacementSettings
     @Binding var shouldShowModal: Bool
+    @State private var showBrowse: Bool = false
     var body: some View {
         ZStack(alignment: .bottom) {
             ARViewContainer()
-            ControlView(
-                shouldShowModal: $shouldShowModal
-            )
+            if self.placementSettings.selectedModel == nil {
+                ControlView(
+                    shouldShowModal: $shouldShowModal,
+                    showBrowse: $showBrowse
+                )
+            }else{
+                PlacementView()
+            }
         }.edgesIgnoringSafeArea(.all)
     }
 }
 
 struct  ARViewContainer: UIViewRepresentable {
-    func makeUIView(context: Context) -> ARView{
-        let arView = ARView(frame: .zero)
+    @EnvironmentObject var placementSettings: PlacementSettings
+    func makeUIView(context: Context) -> CustomARView{
+        let arView = CustomARView(frame: .zero)
+        self.placementSettings.sceneObserver = arView.scene.subscribe(to: SceneEvents.Update.self, {(event) in
+            self.updateScene(for: arView)
+        })
         return arView
     }
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: CustomARView, context: Context) {}
+    
+    private func updateScene(for arView: CustomARView){
+        arView.focusEntity?.isEnabled = self.placementSettings.selectedModel != nil
+        if let confirmedModel = self.placementSettings.confirmedModel, let modeEntity = confirmedModel.modelEntity {
+            self.place(modeEntity, in: arView)
+            self.placementSettings.confirmedModel = nil
+        }
+    }
+    
+    private func place(_ modelEntity: ModelEntity, in arView: ARView){
+        let clonedEntity = modelEntity.clone(recursive: true)
+        
+        clonedEntity.generateCollisionShapes(recursive: true)
+        arView.installGestures([.translation, . rotation], for: clonedEntity)
+        
+        let anchorEntity = AnchorEntity(plane: .any)
+        anchorEntity.addChild(clonedEntity)
+        
+        arView.scene.addAnchor(anchorEntity)
+        
+    }
 }
---
+++ Augmented Reality/Model.swift
@@ -0,0 +1,84 @@
+//
+//  Model.swift
+//  Augmented Reality
+//
+//  Created by sam.y.so on 30/3/2021.
+//
+
+import Foundation
+import RealityKit
+import Combine
+import UIKit.UIImage
+
+enum ModelCategory: CaseIterable {
+    case fire
+    case water
+    case electronic
+
+    
+    var label: String {
+        get {
+            switch self {
+            case .fire:
+                return "Fire"
+            case .water:
+                return "Water"
+            case .electronic:
+                return "Electricity"
+            }
+        }
+    }
+}
+
+class Model {
+    var name: String
+    var category: ModelCategory
+    var thumbnail: UIImage
+    var modelEntity: ModelEntity?
+    var scaleCompensation: Float
+    private var cancellable: AnyCancellable?
+    init(name: String, category: ModelCategory, scaleCompensation: Float = 1.0) {
+        self.name = name
+        self.category = category
+        self.thumbnail = UIImage(named: name) ?? UIImage(systemName: "photo")!
+        self.scaleCompensation = scaleCompensation
+    }
+    func asyncLoadModelEntity() {
+        let filename = self.name + ".usdz"
+        self.cancellable = ModelEntity.loadModelAsync(named: filename)
+            .sink(receiveCompletion: { loadCompletion in
+                switch loadCompletion {
+                case .failure(let error):
+                    print("Unable to load \(filename), Error: \(error)")
+                case .finished:
+                    print("\(filename)")
+                    break
+                }
+            }, receiveValue: { modelEntity in
+                self.modelEntity = modelEntity
+                self.modelEntity?.scale *= self.scaleCompensation
+            })
+    }
+    
+}
+
+struct Models {
+    var all: [Model] = []
+    init() {
+        // fire
+        let fire = Model(name: "fire_selection", category: .fire, scaleCompensation: 0.32/100)
+        self.all += [fire]
+        
+        let water = Model(name: "water_selection", category: .water, scaleCompensation: 0.32/100)
+        self.all += [water]
+
+        // electronic
+        let electronic = Model(name: "electronic_selection", category: .electronic, scaleCompensation: 0.01)
+
+        self.all += [electronic]
+
+    }
+    func get(category: ModelCategory) -> [Model]{
+        return all.filter({$0.category==category})
+    }
+}
