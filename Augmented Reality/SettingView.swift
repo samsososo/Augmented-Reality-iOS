2771a9bf8b64a04621156255b20200e33aa6bff2
SamSo <49915235+samsososo@users.noreply.github.com>
2021-04-01 10:15:39 +0800

    setting page setup (#4)

--- Augmented Reality/SettingView.swift
+++ Augmented Reality/SettingView.swift
@@ -9,11 +9,46 @@ import Foundation
 import SwiftUI
 
 struct SettingsView: View {
+    
+    // 1.
+    @State private var enableAirplaneMode = false
+      
+    // 2.
+    var notificationMode = ["Lock Screen", "Notification Centre", "Banners"]
+    @State private var selectedMode = 0
+    
     var body: some View {
-     NavigationView {
-         ScrollView {
-          Text("hi")
-         }.navigationTitle("Setting")
-     }
+        NavigationView {
+            // 3.
+            Form {
+                // 4.
+                Section(header: Text("General Settings")){
+                    Toggle(isOn: $enableAirplaneMode) {
+                        Text("Airplane Mode")
+                    }
+                    
+                    Picker(selection: $selectedMode, label: Text("Notifications")) {
+                        ForEach(0..<notificationMode.count) {
+                            Text(self.notificationMode[$0])
+                        }
+                    }
+                }
+                
+                // 5.
+                Section(header: Text("About")) {
+                    HStack {
+                        Text("Name")
+                        Spacer()
+                        Text("Education Purpose")
+                    }
+                    
+                    HStack {
+                        Text("Software Version")
+                        Spacer()
+                        Text("1.0.0")
+                    }
+                }
+            } .navigationBarTitle("Settings")
+        }
     }
 }
